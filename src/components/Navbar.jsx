40c20aad6fbc978640c039091503f5004d2baa85
seema971 <134820641+seema971@users.noreply.github.com>
2025-02-25 12:42:39 +0545

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
-import { Button } from "@/components/ui/button"; 
+import { Button } from "./Button"; 
 import { Menu, X, PawPrint, User, Heart, Search, Home } from "lucide-react";
 import "./Navbar.css";
 
